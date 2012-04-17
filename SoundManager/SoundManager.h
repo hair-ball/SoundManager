61f027178d115499769a3ee5ee47c3d9d66f61a6
undancer <undancer@gmail.com>
2012-04-17 11:19:00 +0800

    Initial Commit

---
+++ SoundManager/SoundManager.h
@@ -0,0 +1,13 @@
+//
+//  SoundManager.h
+//  SoundManager
+//
+//  Created by 涛 魏 on 12-4-17.
+//  Copyright (c) 2012年 北京. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundManager : NSObject
+
+@end
---
+++ SoundManager/SoundManager.m
@@ -0,0 +1,13 @@
+//
+//  SoundManager.m
+//  SoundManager
+//
+//  Created by 涛 魏 on 12-4-17.
+//  Copyright (c) 2012年 北京. All rights reserved.
+//
+
+#import "SoundManager.h"
+
+@implementation SoundManager
+
+@end
